69a16460d65e6c53b084306748903c9f020b3c18
a.symeonidis <a.symeonidis@intralot.com>
2021-10-04 21:44:56 +0300

    ask animal to do a trick

--- src/main/java/com/virtualzoo/controllers/AnimalsController.java
+++ src/main/java/com/virtualzoo/controllers/AnimalsController.java
@@ -2,25 +2,36 @@ package com.virtualzoo.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.virtualzoo.models.dto.AnimalResponse;
+import com.virtualzoo.models.dto.TrickResponse;
 import com.virtualzoo.services.AnimalService;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+
+//todo: add swagger
 
 @RestController
-@RequestMapping("myZoo")
+@RequestMapping("myZoo/animals")
 public class AnimalsController {
 
 	@Autowired
 	private AnimalService animalService;
 
-	@GetMapping("/animals/all")
+	@GetMapping("/all")
 	public Flux<AnimalResponse> getAnimals(@RequestParam(required = false) boolean orderBySpecies) {
 		return animalService.getAnimals(orderBySpecies);
+	}
+
+	@GetMapping("/{animalId}/doTrick")
+	public Mono<TrickResponse> getTricks(@PathVariable("animalId") String animalId) {
+		return animalService.getAnimalTricks(animalId);
 	}
 
 }
